e2d2d5a5d22d3a1ba94216fd627dfb42f85779c9
MarinoDrazic <marinodrazic12@gmail.com>
2019-04-19 13:20:56 +0200

    Finished up Main Feed part 2

--- app/src/main/java/com/example/bootleginstagram/BootlegInstagramApi.java
+++ app/src/main/java/com/example/bootleginstagram/BootlegInstagramApi.java
@@ -6,7 +6,7 @@ import retrofit2.Call;
 import retrofit2.http.GET;
 
 public interface BootlegInstagramApi {
-    @GET("photos/?client_id=d19195f7799ac9b6046a1a4392790e3f5643ba35a2933ab38046e4dae862cb38")
+    @GET("photos?per_page=20&client_id=d19195f7799ac9b6046a1a4392790e3f5643ba35a2933ab38046e4dae862cb38")
     Call<List<InstagramUsers>> GetThemUsers();
 
 }
