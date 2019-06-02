ecf71d90bf4a9e68f9670e1c9d51ccedffedb077
MarinoDrazic <marinodrazic12@gmail.com>
2019-06-02 11:08:21 +0200

    Completed Fragmentation MainFeed and UserProfile

--- app/src/main/java/com/example/bootleginstagram/MainActivity.java
+++ app/src/main/java/com/example/bootleginstagram/MainActivity.java
@@ -14,9 +14,7 @@ import retrofit2.Retrofit;
 import retrofit2.converter.gson.GsonConverterFactory;
 
 public class MainActivity extends AppCompatActivity {
-    public BootlegInstagramApi bootlegInstagramApi;
-    BootlegViewModel bootlegViewModel = new BootlegViewModel();
-    RecyclerView recyclerView;
+
     private ViewPager viewPager;
     private SectionsPageAdapter sectionsPageAdapter;
     @Override
@@ -30,25 +28,11 @@ public class MainActivity extends AppCompatActivity {
         TabLayout tabLayout = findViewById(R.id.tabLayout);
         tabLayout.setupWithViewPager(viewPager);
 
-        recyclerView=findViewById(R.id.rvFeed);
-        OkHttpClient client = new OkHttpClient.Builder()//
-                .build();
-        Retrofit retrofit = new Retrofit.Builder()
-                .client(client)
-                .baseUrl("https://api.unsplash.com/")
-                .addConverterFactory(GsonConverterFactory.create())
-                .build();
-        bootlegInstagramApi = retrofit.create(BootlegInstagramApi.class);
-        bootlegViewModel.fetchData(bootlegInstagramApi);
-        bootlegViewModel.getThemPickm8().observe(this, users -> initRecyclerView(users) );
+
+
 
     }
-    private void initRecyclerView(List<InstagramUsers> users){
-        RecyclerViewAdapter adapter = new RecyclerViewAdapter(users,this);
-        recyclerView.setAdapter((adapter));
-        recyclerView.setLayoutManager(new LinearLayoutManager(this));
 
-    }
     private void setupViewPager(ViewPager viewPager)
     {
         SectionsPageAdapter adapter = new SectionsPageAdapter(getSupportFragmentManager());
